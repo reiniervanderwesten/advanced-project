d9b944cd848a552a12ac5927ebbe7d33a5c5dae2
naam <reiniervanderwesten@hotmail.com>
2025-05-05 08:40:00 +0200

    import event

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Text, Image } from '@chakra-ui/react';
+import {Link as ReactRouterLink, useLoaderData, useNavigate} from 'react-router-dom';
+import { useState } from 'react';
 
 export const EventPage = () => {
   return <Heading>Event</Heading>;
