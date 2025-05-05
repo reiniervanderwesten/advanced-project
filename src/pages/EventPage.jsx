df9cd92c758bdb2a3bb0f5ce7c19965a161037f2
naam <reiniervanderwesten@hotmail.com>
2025-05-05 09:55:57 +0200

    event page basis

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Heading, Text, Image, Flex } from '@chakra-ui/react';
+import { Heading, Text, Image, Flex, Box } from '@chakra-ui/react';
 import {Link as ReactRouterLink, useLoaderData, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
 
 export const loader=async({params})=>{
-  const users=await fetch(`http://localhost:3000/users`);
+  const users=await fetch("http://localhost:3000/users");
   const event= await fetch(`http://localhost:3000/events/${params.eventId}`);
 
   return {users: await users.json(), event: await event.json()};
 };
 
 export const EventPage = () => {
+  const {users, event}=useLoaderData();
   return (
     <Flex h="fit-content" w="100" flexDir={'column'} mt={2}>
       <Heading m={4}>Event</Heading>
-      <Text>g</Text>
+      <Box alignSelf={'center'}>
+        <Text>{event.title}</Text>
+
+      </Box>
+      
 
     </Flex>
 
