ce0c6577154e13f685fa265e1a71c46aaef545d1
naam <reiniervanderwesten@hotmail.com>
2025-05-09 20:45:10 +0200

    form skelet

--- src/pages/NewEvent.jsx
+++ src/pages/NewEvent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Heading, Input, Text, NumberInput, NumberInputField, Button} from '@chakra-ui/react';
+import {Heading, Input, Text, NumberInput, NumberInputField, Button, Stack} from '@chakra-ui/react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,38 +16,35 @@ export const NewEvent=()=>{
     const navigate= useNavigate();
 
     
-
-
-    
-
-    
-
-    
-
-
-
-    
-    
-
     
     
 
     return (
-        <>
-
+        <Stack padding={3} flexDirection={'column'}>
             <Heading>New Event</Heading>
-            
-            
-            
 
-            
-            
-            
-            
-            
+            <form>
+                <NumberInput>
+                    <NumberInputField variant={'outline'} placeholder={'number created by'} w= {250} value={created} onChange={e=>setCreated(e.target.value)} required="required"/>
+                </NumberInput>
+
+                <Input display= {'inline-block'} variant={'outline'} type="text" placeholder={'title'} w={250} value={title} onChange={e=>setTitle(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type="text" placeholder={'description'} w={250} value={description} onChange={e=>setDescription(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type='url' placeholder={'link to image'} w={250} value={image} onChange={e=>setImage(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type='text' placeholder={'belongs to category(ies)'} w={250} value={category} onChange={e=>setCategory(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type='text' placeholder={'location'} w={250} value={location} onChange={e=>setLocation(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type={'datetime-local'}  w={250} value={startTime} onChange={e=>setStartTime(e.target.value)} required="required"></Input>
+
+                <Input variant={'outline'} type={'datetime-local'} w={250} value={endTime} onChange={e=>setEndTime(e.target.value)} required="required"></Input>
+                <Button margin={4} type="submit">Add Event</Button>
+            </form>
+        </Stack>
         
-        
-        </>
         
         
         
